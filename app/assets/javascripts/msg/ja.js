73708171386d9e1c771a1bde0bec6c6603f36e03
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2015-09-11 15:14:55 +0900

    add change_pen_color　block

--- app/assets/javascripts/msg/ja.js
+++ app/assets/javascripts/msg/ja.js
@@ -316,6 +316,7 @@ Blockly.Msg.BLOCKS_OPERATORS_FALSE = '偽';
 Blockly.Msg.BLOCKS_PEN_DOWN_PEN = '●ペンを下ろす';
 Blockly.Msg.BLOCKS_PEN_UP_PEN = '●ペンを上げる';
 Blockly.Msg.BLOCKS_PEN_SET_PEN_COLOR = '●ペンの色を%1にする';
+Blockly.Msg.BLOCKS_PEN_CHANGE_PEN_COLOR_BY = '●ペンの色を%1ずつ変える';
 Blockly.Msg.BLOCKS_PEN_SET_PEN_COLOR_TO = '●ペンの色を%1にする';
 Blockly.Msg.BLOCKS_PEN_SET_PEN_SHADE_TO = '●ペンの濃さを%1にする';
 
